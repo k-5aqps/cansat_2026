4f40702d0f65cc5e1a5ebfbdbc8ad773c1f15bc6
k-5aqps <perseus.keigo55@gmail.com>
2026-01-13 16:42:41 +0900

    クラス化

--- src/logwrite.py
+++ src/logwrite.py
@@ -1,23 +1,27 @@
 import logging
 import logging.config
-with open('logconfig.ini','r',encoding='utf-8') as f:
-    logging.config.fileConfig(f)
-logger = logging.getLogger('root')
 
-def write(log,loglevel):
-    if loglevel=="DEBUG":
-        logger.debug(f"{log}")
-    elif loglevel=="INFO":
-        logger.info(f"{log}")
-    elif loglevel=="WARNING":
-        logger.warning(f"{log}")
-    elif loglevel=="ERROR":
-        logger.error(f"{log}")
-    elif loglevel=="CRITICAL":
-        logger.critical(f"{log}")
+class MyLogging():
+    def __init__(self):
+        with open('logconfig.ini','r',encoding='utf-8') as f:
+            logging.config.fileConfig(f)
+        self.logger = logging.getLogger('root')
+
+    def write(self,log,loglevel):
+        if loglevel=="DEBUG":
+            self.logger.debug(f"{log}")
+        elif loglevel=="INFO":
+            self.logger.info(f"{log}")
+        elif loglevel=="WARNING":
+            self.logger.warning(f"{log}")
+        elif loglevel=="ERROR":
+            self.logger.error(f"{log}")
+        elif loglevel=="CRITICAL":
+            self.logger.critical(f"{log}")
 
 def main():
-    write("k-5","ERROR")
+    log=MyLogging()
+    log.write("k-5","ERROR")
 
 if __name__ == "__main__":
     main()
