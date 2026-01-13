4dc5c725d84f7f1b38d396c7d463a8c593d09a97
k-5aqps <perseus.keigo55@gmail.com>
2026-01-13 15:51:31 +0900

    クラス化かつ画像をBGRで返却化

---
+++ src/picamera2_t1.py
@@ -0,0 +1,35 @@
+from picamera2 import Picamera2
+import cv2
+import numpy as np
+
+class CameraModule:
+    def __init__(self):
+        self.picam2=Picamera2()
+        self.picam2.configure(self.picam2.create_still_configuration(main={"size": (1280, 720)}))
+        self.cnt=0
+        self.picam2.start()
+    
+    def cap(self):
+        img=self.picam2.capture_array()
+        self.cnt+=1
+        self.save(img)
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        self.save_bgr(img_bgr)
+        return img_bgr
+    
+    def save(self,img):
+        cv2.imwrite(f"/home/pi/img/{self.cnt}.jpeg",img)
+
+    def save_bgr(self,img_bgr):
+        cv2.imwrite(f"/home/pi/img/{self.cnt}_BGR.jpeg",img_bgr) #保存した画像の確認。いらない画像は削除
+
+    def end(self):
+        self.picam2.stop()
+        self.picam2.close()
+
+def main():
+    camera = CameraModule()
+    camera.cap()
+
+if __name__=="__main__":
+    main()
