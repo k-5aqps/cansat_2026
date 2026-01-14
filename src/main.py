65ac4a8748d38ec258c59c1cee99a4baee13b0a3
k-5aqps <perseus.keigo55@gmail.com>
2026-01-14 19:12:07 +0900

    オブジェクト化

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 import pyserial_t1
+import picamera2_t1
+import img_dtc_t1
+import logwrite
+import motor
+
+gps=pyserial_t1.GPSModule()
+cm=picamera2_t1.CameraModule()
+img=img_dtc_t1.Detect()
+log=logwrite.MyLogging()
+mv=motor.MotorController()
+
+def main():
+    mv.move("forward",4,80)
