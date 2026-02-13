fc16bc966265190647c1ff3e9395db08b951b49e
k-5aqps <perseus.keigo55@gmail.com>
2026-02-14 00:15:29 +0900

    パスの変更とcsvファイルの出力

--- src/logwrite.py
+++ src/logwrite.py
@@ -1,9 +1,12 @@
 import logging
 import logging.config
+import csv
+import datetime
+
 
 class MyLogging():
     def __init__(self):
-        with open('logconfig.ini','r',encoding='utf-8') as f:
+        with open('../config/logconfig.ini','r',encoding='utf-8') as f:
             logging.config.fileConfig(f)
         self.logger = logging.getLogger('root')
 
@@ -18,6 +21,21 @@ class MyLogging():
             self.logger.error(f"{log}")
         elif loglevel=="CRITICAL":
             self.logger.critical(f"{log}")
+
+    def init():
+        with open("../log/gpslog.csv","w") as f:
+            pass
+        with open("../log/degree.csv","w") as f:
+            pass
+
+    def forCSV(self,lat,lon):
+        with open("../log/gpslog.csv","a") as f:
+            wrt = csv.writer(f)
+            wrt.writerow([datetime.datetime.now(),lat,lon])
+    def forLATLON(self,temp1,temp2):
+        with open("../log/degree.csv","a") as f:
+            wrt = csv.writer(f)
+            wrt.writerow([datetime.datetime.now(),temp1,temp2])
 
 def main():
     log=MyLogging()
