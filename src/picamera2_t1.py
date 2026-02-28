9e5276c95612ade48699464b4bc0f3474b01656f
k-5aqps <perseus.keigo55@gmail.com>
2026-02-28 20:22:00 +0900

    撮影の安定化、コードの改良

--- src/picamera2_t1.py
+++ src/picamera2_t1.py
@@ -1,4 +1,5 @@
 from picamera2 import Picamera2
+import time
 import cv2
 import numpy as np
 
@@ -10,18 +11,20 @@ class CameraModule:
         self.picam2.start()
     
     def cap(self):
+        time.sleep(1)
         img=self.picam2.capture_array()
+        img=cv2.flip(img,-1)
         self.cnt+=1
-        self.save(img)
+        #self.save(img)
         img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         self.save_bgr(img_bgr)
         return img_bgr
     
-    def save(self,img):
-        cv2.imwrite(f"/home/pi/img/{self.cnt}.jpeg",img)
+    #def save(self,img):
+    #    cv2.imwrite(f"/home/pi/cansat_2026/img/test_cap/{self.cnt}.jpeg",img)
 
     def save_bgr(self,img_bgr):
-        cv2.imwrite(f"/home/pi/img/{self.cnt}_BGR.jpeg",img_bgr) #保存した画像の確認。いらない画像は削除
+        cv2.imwrite(f"/home/pi/cansat_2026/img/test_cap/{self.cnt}_BGR.jpeg",img_bgr) #保存した画像の確認。いらない画像は削除
 
     def end(self):
         self.picam2.stop()
